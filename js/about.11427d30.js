687ec9aa40ec4628e93f740f330570cbfbdbc1ac
hermanl <lucian.herman@ppb.com>
2021-08-26 17:12:39 +0300

    add counter

---
+++ js/about.11427d30.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var u=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Site under construction")])])}],r=n("2877"),s={},a=Object(r["a"])(s,u,c,!1,null,null,null);e["default"]=a.exports}}]);
+//# sourceMappingURL=about.11427d30.js.map
---
+++ precache-manifest.d578b89b76cb3ab7aac0d55db021847d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "827ef2f6217348c9d0dd",
+    "url": "/js/about.11427d30.js"
+  },
+  {
+    "revision": "c9a215d394c6d0fe219f",
+    "url": "/css/app.1769627b.css"
+  },
+  {
+    "revision": "c9a215d394c6d0fe219f",
+    "url": "/js/app.84789e61.js"
+  },
+  {
+    "revision": "659cdda1aa1dcba798c4",
+    "url": "/js/chunk-vendors.8df963c3.js"
+  },
+  {
+    "revision": "ea1497eca14e4c7ebede12572116c019",
+    "url": "/img/baby.ea1497ec.png"
+  },
+  {
+    "revision": "69c132c739dd82e38a479c7757a1d673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
